bacc376fd8417e4366c47d56469c23bb609ee142
FranCiscoBM12 <162335182+FranCiscoBM12@users.noreply.github.com>
2024-03-14 16:15:52 +0100

    proyecto creado Persona

---
+++ Personas/src/Persona.java
@@ -0,0 +1,105 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    public String dni;
+
+    /**
+     * 
+     */
+    public String nombre;
+
+    /**
+     * 
+     */
+    public String apellidos;
+
+    /**
+     * 
+     */
+    public int Edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param Edad
+     */
+    public void Persona(String dni, String nombre, String apellidos, int Edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @return Devuelve el DNI
+     */
+    public String getDNI() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDNI(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return Devuelve el nombre
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param nombre
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * @return Devuelve el apellido
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param apellidos
+     */
+    public void setApellidos(String apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * @return Devuelve la edad
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Edad
+     */
+    public void setEdad(int Edad) {
+        // TODO implement here
+    }
+
+}
