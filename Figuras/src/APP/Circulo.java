b3665688b020d5066f6804505ecc975582df1882
FranCiscoBM12 <162335182+FranCiscoBM12@users.noreply.github.com>
2024-03-19 17:50:02 +0100

    Triangulo añadido

---
+++ Figuras/src/APP/Circulo.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Circulo {
+
+    /**
+     * Default constructor
+     */
+    public Circulo() {
+    }
+
+    /**
+     * 
+     */
+    public int radio;
+
+    /**
+     * @param radio
+     */
+    public void circulo(int radio) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getRadio() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param radio
+     */
+    public void setRadio(int radio) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getArea() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int getPerimetro() {
+        // TODO implement here
+        return 0;
+    }
+
+}
---
+++ Figuras/src/APP/Triangulo.java
@@ -0,0 +1,79 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Triangulo {
+
+    /**
+     * Default constructor
+     */
+    public Triangulo() {
+    }
+
+    /**
+     * 
+     */
+    public int base;
+
+    /**
+     * 
+     */
+    public int altura;
+
+    /**
+     * @return
+     */
+    public double getArea() {
+        // TODO implement here
+        return 0.0;
+    }
+
+    /**
+     * @param Base 
+     * @param Altura
+     */
+    public void Triangulo(int Base, int Altura) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getBase() {
+        // TODO implement here
+        return this.base;
+    }
+
+    /**
+     * @param Base
+     */
+    public void setBase(int Base) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getAltura() {
+        // TODO implement here
+        return this.altura;
+    }
+
+    /**
+     * @param Altura
+     */
+    public void setAltura(int Altura) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public double getPerimetro() {
+        // TODO implement here
+        return 0.0;
+    }
+
+}
