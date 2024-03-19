b8c9899477f487b66c9388ce8fb1bf58b9eabd17
FranCiscoBM12 <162335182+FranCiscoBM12@users.noreply.github.com>
2024-03-19 16:19:04 +0100

    Rectangulo implementado

---
+++ Personas/src/Rectangulo.java
@@ -0,0 +1,71 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Rectangulo {
+
+    /**
+     * Default constructor
+     */
+    public Rectangulo() {
+    }
+
+    /**
+     * 
+     */
+    private int base;
+
+    /**
+     * 
+     */
+    private int altura;
+
+    /**
+     * @return
+     */
+    private int getBase() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Base
+     */
+    public void setBase(int Base) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getAltura() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Altura
+     */
+    public void setAltura(int Altura) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Base 
+     * @param Altura
+     */
+    public void Rectangulo(int Base, int Altura) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public int getArea() {
+        // TODO implement here
+        return 0;
+    }
+
+}
