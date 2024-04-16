e2cc2579d1c8dafe829a2011b46f0ffea4796977
FranCiscoBM12 <162335182+FranCiscoBM12@users.noreply.github.com>
2024-04-16 17:39:29 +0200

    Primer metodo

--- PracticaAlgoritmos/src/app/Pricipal.java
+++ PracticaAlgoritmos/src/app/Pricipal.java
@@ -2,50 +2,15 @@ package app;
 import util.Algoritmos;
 import java.util.Random;
 
+/**
+ * @author Medac Tarde
+ * 
+ * clase Principal,se ocupa de probar las funciones de la clase Algoritmos
+ */
+
 public class Pricipal {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-        int[] numeros = new int[5];
-
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt(10) + 1;
-        }
-
-        System.out.println("Array de números aleatorios:");
-        for (int num : numeros) {
-        }
-		
-        int[] resultadoFibonacci = new int[numeros.length];
-
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoFibonacci[i] = Algoritmos.fibonacci(numeros[i]);
-        }
-
-        System.out.println("Array de números de Fibonacci:");
-        for (int num : resultadoFibonacci) {
-        }
-        
-        int[] resultadoFactorial = new int[numeros.length];
-
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoFactorial[i] = Algoritmos.factorial(numeros[i]);
-        }
-
-        System.out.println("Array de números factorial:");
-        for (int num : resultadoFactorial) {
-
-        }
-        
-        boolean[] resultadoPrimos = new boolean[numeros.length];
-
-        for (int i = 0; i < numeros.length; i++) {
-            resultadoPrimos[i] = Algoritmos.esPrimo(numeros[i]);
-        }
-
-        System.out.println("Array de resultados de números primos:");
-        for (boolean esPrimo : resultadoPrimos) {
-        }
 	
 	}
 
--- PracticaAlgoritmos/src/util/Algoritmos.java
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -1,6 +1,11 @@
 package util;
 
 public abstract class Algoritmos {
+	/**
+	 * 
+	 * @param n
+	 * @return devuelve el numero fibonacci de n
+	 */
 	public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -16,28 +21,6 @@ public abstract class Algoritmos {
             return b;
         }
         }
-	public static int factorial(int numero) {
-		int factorial = 1;
-		for(int i=1;i<=numero;i++) {
-			factorial*=i;		
-		}
-		return factorial;
-		
-	}
-
-	public static boolean esPrimo(int numero) {
-		if (numero <= 1) {
-            return false;
-        }
-        if (numero == 2) {
-            return true;
-        }
-        for (int i = 2; i <= Math.sqrt(numero); i++) {
-            if (numero % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	
 	
 }
